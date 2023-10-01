0bc9fd5f332efc2785ce480ebcc81a1d0d878e2e
aerial-301 <75935539+aerial-301@users.noreply.github.com>
2023-10-01 15:02:32 +0300

    More assets

--- src/assets.js
+++ src/assets.js
@@ -1,10 +1,26 @@
 export const png = {
   "bg": "bg",
+  "block": "block",
+  "bullet": "bullet",
+  "defender": "defender",
   "nextButton": "nextButton",
+  "payButton": "payButton",
   "ssd": "ssd",
-  "topaz_0": "topaz_0"
+  "topaz_0": "topaz_0",
+  "virus": "virus"
 }
-export const ogg = {}
+export const ogg = {
+  "accept": "accept",
+  "can_not": "can_not",
+  "confirm": "confirm",
+  "dismiss": "dismiss",
+  "eject": "eject",
+  "hover": "hover",
+  "tap": "tap"
+}
+export const wav = {
+  "BGM": "BGM"
+}
 export const fnt = {
   "topaz": "topaz"
 }
